e38b517192c60acc3e7b9a57af64a16f29465deb
Sven Goldstein <s.goldstein@beckhoff.com>
2020-07-15 17:50:04 +0200

    modified update workflow

--- user/InstallUpdateAgent.ps1
+++ user/InstallUpdateAgent.ps1
@@ -9,55 +9,80 @@ $apiUrl = "$repoUrlApi/releases/$Version"
 $releaseInfo = Invoke-RestMethod -Uri $apiUrl
 $selectedVersion = $releaseInfo.tag_name
 
-# Build download URL and temporary target path
-$downloadUrl = "$repoUrl/releases/download/$selectedVersion/$releaseBinaryName"
-$tempDirectory = "C:\Temp"
-$tempFile = "$tempDirectory\$releaseBinaryName"
-
-if (-not(Test-Path $tempDirectory)) {
-    New-Item -Path $tempDirectory -ItemType "directory"
-}
-
-if (Test-Path $tempFile) {
-    Remove-Item -Path $tempFile
-}
-
-# Download release binary via BITS
-Invoke-Expression -Command "bitsadmin /transfer TcCloudEngineeringAgentUpdate /dynamic /download /priority FOREGROUND $downloadUrl $tempFile"
-
-# Stop TwinCAT Cloud Engineering Agent service
-$serviceName = "TcCloudEngineeringAgent"
-if (-not (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) -eq $null) {
-    Stop-Service -Name $serviceName
-}
-
-# Check if Agent directory already exists - if not, then create it
-$agentDirectory = "C:\Program Files (x86)\Beckhoff Automation\TcCloudEngineeringAgent"
-if (-not (Test-Path $agentDirectory)) {
-    New-Item -Path $agentDirectory -ItemType "directory"
-}
-
-# Remove existing Agent files
-Remove-Item -Recurse -Force $agentDirectory
-
-# Extract archive to Agent directory
-Expand-Archive -Path $tempFile $agentDirectory
-
-# Write installed Agent version to Windows Registry
+# Read currently installed Agent version from Windows Registry and compare with latest version on remote repo
 $regKey = "HKLM:\SOFTWARE\WOW6432Node\Beckhoff\TwinCAT Cloud Engineering"
 $regKeyAgentProp = "AgentVersion"
+$install = $false
 if (Test-Path $regKey) {
-    $prop = Get-ItemProperty -Path $regKey -Name $regKeyAgentProp
-    if ($prop -eq $null) {
-        New-ItemProperty -Path $regKey -Name $regKeyAgentProp -Value $selectedVersion
+    $installedVersion = Get-ItemProperty -Path $regKey -Name $regKeyAgentProp
+    if (-not ($installedVersion -eq $null)) {
+        # Existing Agent installation found -> check if latest version on remote repo is newer
+        $selectedVersionObj = [version]$selectedVersion
+        $installedVersionObj = [version]$installedVersion.AgentVersion
+        if ($selectedVersionObj -gt $installedVersionObj) {
+            # Latest release version is newer -> install
+            $install = $true
+        }
     }
     else {
-        Remove-ItemProperty -Path $regKey -Name $regKeyAgentProp
-        New-ItemProperty -Path $regKey -Name $regKeyAgentProp -Value $selectedVersion
+        # No Agent has been installed -> install
+        $install = $true
     }
 }
 
-# Start TwinCAT Cloud Engineering Agent service
-if (-not (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) -eq $null) {
-    Start-Service -Name $serviceName
+# Only install if latest version on repo is newer or if no Agent has been installed at all
+
+if ($install) {
+
+    # Build download URL and temporary target path
+    $downloadUrl = "$repoUrl/releases/download/$selectedVersion/$releaseBinaryName"
+    $tempDirectory = "C:\Temp"
+    $tempFile = "$tempDirectory\$releaseBinaryName"
+
+    if (-not(Test-Path $tempDirectory)) {
+        New-Item -Path $tempDirectory -ItemType "directory"
+    }
+
+    if (Test-Path $tempFile) {
+        Remove-Item -Path $tempFile
+    }
+
+    # Download release binary via BITS
+    Invoke-Expression -Command "bitsadmin /transfer TcCloudEngineeringAgentUpdate /dynamic /download /priority FOREGROUND $downloadUrl $tempFile"
+
+    # Stop TwinCAT Cloud Engineering Agent service
+    $serviceName = "TcCloudEngineeringAgent"
+    if (-not (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) -eq $null) {
+        Stop-Service -Name $serviceName
+    }
+
+    # Check if Agent directory already exists - if not, then create it
+    $agentDirectory = "C:\Program Files (x86)\Beckhoff Automation\TcCloudEngineeringAgent"
+    if (-not (Test-Path $agentDirectory)) {
+        New-Item -Path $agentDirectory -ItemType "directory"
+    }
+
+    # Remove existing Agent files
+    Remove-Item -Recurse -Force $agentDirectory
+
+    # Extract archive to Agent directory
+    Expand-Archive -Path $tempFile $agentDirectory
+
+    # Write installed Agent version to Windows Registry
+    if (Test-Path $regKey) {
+        $prop = Get-ItemProperty -Path $regKey -Name $regKeyAgentProp
+        if ($prop -eq $null) {
+            New-ItemProperty -Path $regKey -Name $regKeyAgentProp -Value $selectedVersion
+        }
+        else {
+            Remove-ItemProperty -Path $regKey -Name $regKeyAgentProp
+            New-ItemProperty -Path $regKey -Name $regKeyAgentProp -Value $selectedVersion
+        }
+    }
+
+    # Start TwinCAT Cloud Engineering Agent service
+    if (-not (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) -eq $null) {
+        Start-Service -Name $serviceName
+    }
+
 }
