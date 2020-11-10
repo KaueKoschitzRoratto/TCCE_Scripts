c62397debfcd75010775015ddd6a04899f96084a
Sven Goldstein <s.goldstein@beckhoff.com>
2020-11-10 13:46:37 +0100

    removed ssh admin user account because it is obsolete

--- init/CreateUserSsh.ps1
+++ init/CreateUserSsh.ps1
@@ -19,17 +19,11 @@ function Scramble-String([string]$inputString){
 }
 
 $username = "Tcce_User_SSH"
-$usernameAdmin = "Tcce_User_SSHAdmin"
 $groupName = "Tcce_Group_SSH"
-$groupNameAdmin = "Tcce_Group_SSHAdmins"
 
 $password = Get-RandomCharacters -length 12 -characters 'abcdefghiklmnoprstuvwxyzABCDEFGHKLMNOPRSTUVWXYZ1234567890!$%&()=?@#+'
 $password = Scramble-String($password)
 $passwordSec = ConvertTo-SecureString -String $password -AsPlainText -Force
-
-$passwordAdmin = Get-RandomCharacters -length 12 -characters 'abcdefghiklmnoprstuvwxyzABCDEFGHKLMNOPRSTUVWXYZ1234567890!$%&()=?@#+'
-$passwordAdmin = Scramble-String($passwordAdmin)
-$passwordSecAdmin = ConvertTo-SecureString -String $passwordAdmin -AsPlainText -Force
 
 # Create new user account if it does not exist
 $account = Get-LocalUser -Name $username -ErrorAction SilentlyContinue
@@ -39,14 +33,6 @@ if (-not ($account -eq $null)) {
 $usr = New-LocalUser -Name $username -FullName $username -Description "Account for SSH authentication" -Password $passwordSec
 $grp = Add-LocalGroupMember -Group $groupName -Member $username
 
-# Create new user account if it does not exist
-$account = Get-LocalUser -Name $usernameAdmin -ErrorAction SilentlyContinue
-if (-not ($account -eq $null)) {
-    $rmv = Remove-LocalUser -Name $usernameAdmin
-}
-$usr = New-LocalUser -Name $usernameAdmin -FullName $usernameAdmin -Description "Account for SSH authentication" -Password $passwordSecAdmin
-$grp = Add-LocalGroupMember -Group $groupNameAdmin -Member $usernameAdmin
-
 # Store created user credentials on user's desktop as temporary note
 if (-not (Test-Path -Path "$templateReadmePath\$templateReadmeFile")) {
   $cpy = Copy-Item -Path "$repoPathInitScripts\templates\$templateReadmeFile" -Destination "$templateReadmePath\$templateReadmeFile"
@@ -55,7 +41,5 @@ $readmeContent = Get-Content -Path "$templateReadmePath\$templateReadmeFile" -Ra
 $readmeContent = $readmeContent.Replace("%publicIp%", $PublicIp)
 $readmeContent = $readmeContent.Replace("%usernameSsh%", $username)
 $readmeContent = $readmeContent.Replace("%passwordSsh%", $password)
-$readmeContent = $readmeContent.Replace("%usernameSshAdmin%", $usernameAdmin)
-$readmeContent = $readmeContent.Replace("%passwordSshAdmin%", $passwordAdmin)
 
 $xml = Set-Content -Path $templateReadmePath\$templateReadmeFile -Value $readmeContent
