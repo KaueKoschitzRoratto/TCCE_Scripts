c6497ecf1951182dc440a2be96c24c84bcc225a7
Sven Goldstein <s.goldstein@beckhoff.com>
2020-11-13 14:58:01 +0100

    optimized code dir script for auto removal of existing directories

--- init/CopyCodeDir.ps1
+++ init/CopyCodeDir.ps1
@@ -1,6 +1,7 @@
 $downloadUrl = "https://tcce-downloads.s3.eu-central-1.amazonaws.com/tcflare/TcFlare_Code.zip"
 $tempDirectory = "C:\Temp"
 $tempFile = "$tempDirectory\TcFlare_Code.zip"
+$destDir = "C:\Users\Administrator\Desktop"
 
 if (-not(Test-Path $tempDirectory)) {
 	$dir = New-Item -Path $tempDirectory -ItemType "directory"
@@ -13,6 +14,11 @@ if (Test-Path $tempFile) {
 # Download samples via BITS or Invoke-WebRequest
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri $downloadUrl -OutFile $tempFile
+
+# Remove existing files if there are any
+if (Test-Path "$destDir\code") {
+	$rmv = Remove-Item -Recurse -Force "$destDir\code"
+}
 
 # Extract archive to Administrator Desktop directory
 $zip = Expand-Archive -Path $tempFile "C:\Users\Administrator\Desktop"
